29108e60c21e8f95fedad2ae5fb900dd62f86561
Ronald Salmon <salmon@railroad19.com>
2018-03-27 20:43:58 -0600

    axios

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-var express = require('express');
-var proxy = require('http-proxy-middleware');
+const express = require('express');
+const axios = require('axios')
 
 var app = express();
 
-app.use('/', proxy({
-    target: 'http://bikeindex.org',
-    onProxyRes(proxyResp) {
-        proxyResp.headers['Access-Control-Allow-Origin'] = '*';
-        console.log('headers', proxyResp.headers);
-    },
-    changeOrigin: false
-}));
+app.get('*', (req, res) => {
+    axios.get('https://bikeindex.org')
+        .then(response => {
+            res.json(response);
+        });
+});
+
 app.listen(process.env.PORT || 3000);
