658723a19f43ef95fa4842719f68f7136f63a179
Ronald Salmon <salmon@railroad19.com>
2018-03-27 20:03:55 -0600

    access control

--- index.js
+++ index.js
@@ -3,5 +3,11 @@ var proxy = require('http-proxy-middleware');
 
 var app = express();
 
-app.use('/', proxy({target: 'http://bikeindex.org', changeOrigin: true}));
+app.use('/', proxy({
+    target: 'http://bikeindex.org',
+    onProxyRes(proxyResp) {
+        proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+    },
+    changeOrigin: true
+}));
 app.listen(process.env.PORT);
